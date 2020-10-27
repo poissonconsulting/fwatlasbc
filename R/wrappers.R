5af2dae83739adac2145911c50188256fb4c4a0c
Joe Thorley <joe@poissonconsulting.ca>
2020-10-26 17:45:36 -0700

    fix up

---
+++ R/wrappers.R
@@ -0,0 +1,26 @@
+#' FWA stream network
+#'
+#' Get stream network features from gnis_name.
+#'
+#' @inheritParams fwapgr::fwa_collection
+#' @param gnis_name A character string of the gnis_name.
+#' @return A sf object
+#' @export
+#' @examples
+#' fwa_stream_gnis('Sangan River')
+fwa_stream_gnis <- function(gnis_name,
+                            columns = NULL,
+                            bbox = NULL,
+                            epsg = 4326){
+
+  chk_string(gnis_name)
+  table <- "fwa_named_streams"
+  filter <- list(gnis_name = gnis_name)
+
+  fwapgr::fwa_collection(table = table,
+                 columns = columns,
+                 filter = filter,
+                 bbox = bbox,
+                 epsg = epsg)
+}
+
