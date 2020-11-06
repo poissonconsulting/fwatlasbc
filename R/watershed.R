3dbd8cee96118a227b685abbb8ec59a22d0b6f52
Seb Dalgarno <seb.dalgarno@gmail.com>
2020-11-06 11:29:26 -0800

    document and export watershed funs

--- R/watershed.R
+++ R/watershed.R
@@ -1,3 +1,16 @@
+#' FWA point to watershed
+#'
+#' Get watershed polygon from a sf or sfc point starting from nearest point along a FWA stream.
+#'
+#' @param point A single row sf object or sfc object of the point (sfc geometry must inherit from sfc_POINT).
+#' @return A sf object
+#' @seealso \code{\link[fwapgr]{fwa_watershed_at_measure}} and \code{\link[fwapgr]{fwa_indexpoint}}.
+#' @export
+#' @examples
+#' \dontrun{
+#' sf_point <- sf::st_as_sf(sf::st_as_sfc("POINT(-131.927 54.032)", crs = 4326))
+#' fwa_point_to_watershed(sf_point)
+#' }
 fwa_point_to_watershed <- function(point) {
   chk_sf_sfc(point)
   point <- sf::st_geometry(point)
@@ -7,6 +20,21 @@ fwa_point_to_watershed <- function(point) {
   fwa_xy_to_watershed(x = coords[1], y = coords[2], epsg = epsg)
 }
 
+#' FWA xy to watershed
+#'
+#' Get watershed polygon from x coordinate, y coordinate and epsg starting from nearest point along a FWA stream.
+#'
+#' @inheritParams fwapgr::fwa_collection
+#' @param x A number of the x coordinate.
+#' @param y A number of the y coordinate.
+#' @return A sf object
+#' @seealso \code{\link[fwapgr]{fwa_watershed_at_measure}} and \code{\link[fwapgr]{fwa_indexpoint}}.
+#' @export
+#' @examples
+#' \dontrun{
+#' sf_point <- sf::st_as_sf(sf::st_as_sfc("POINT(-131.927 54.032)", crs = 4326))
+#' fwa_xy_to_watershed(x = -131.927, y = 54.032, epsg = 4326)
+#' }
 fwa_xy_to_watershed <- function(x, y, epsg = getOption("fwa.epsg", 3005)) {
   chk_number(x)
   chk_number(y)
@@ -19,6 +47,19 @@ fwa_xy_to_watershed <- function(x, y, epsg = getOption("fwa.epsg", 3005)) {
   wshed["geometry"]
 }
 
+#' FWA blue_line_key to watershed
+#'
+#' Get watershed polygon from blue_line_kay starting from a distance upstream of the river mouth.
+#'
+#' @inheritParams fwapgr::fwa_locate_along
+#' @param distance_from_mouth A positive whole number of the distance in meters upstream from the river mouth.
+#' @return A sf object
+#' @seealso \code{\link[fwapgr]{fwa_watershed_at_measure}}.
+#' @export
+#' @examples
+#' \dontrun{
+#' fwa_blue_line_key_to_watershed(356308001)
+#' }
 fwa_blue_line_key_to_watershed <- function(blue_line_key, distance_from_mouth = 0,
                                            epsg = getOption("fwa.epsg", 3005)) {
   chk_whole_number(blue_line_key)
