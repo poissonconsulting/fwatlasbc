bd2cf9643bdcffe7ab03ddd30a4bb8a9aca68ace
Joe Thorley <joe@poissonconsulting.ca>
2020-11-11 16:51:17 -0800

    documented fwa_nearest_rkm()

--- R/rkm.R
+++ R/rkm.R
@@ -1,6 +1,6 @@
-#' FWA rkm
+#' Get Rkm
 #'
-#' Provided a blue_line_key and starting distance upstream from river mouth, return Rkm points along an interval.
+#' Provided a blue_line_key and starting distance upstream from river mouth, return river kilometre  points along an interval.
 #'
 #' @param blue_line_key An integer of the stream blue line key.
 #' @param interval An integer of the interval distance in meters.
@@ -8,6 +8,7 @@
 #' @param epsg A positive whole number of the epsg to transform features to.
 #' @param limit A positive whole number of the maximum number of features to return.
 #' @return A sf object
+#' @family rkm
 #' @export
 #' @examples
 #' fwa_rkm(356308001)
@@ -38,10 +39,25 @@ blue_line_key_nearest_rkm <- function(blue_line_key, x, rkm) {
     x <- x[!is.na(x$blue_line_key) & x$blue_line_key == blue_line_key,]
     rkm <- rkm[rkm$blue_line_key == blue_line_key,]
   }
-  print(x)
   nearest_rkm(x, rkm)
 }
 
+#' Nearest Rkm
+#'
+#' Assigns spatial points to river kilometre with matching blue_line_key.
+#' If the blue_line_key is missing then it also assigns the blue_line_key.
+#'
+#' @param x An sf data frame of spatial points with optional blue_line_key column.
+#' @param rkm An sf data frame of spatial point with blue_line_key and rkm columns.
+#'
+#' @return An updated version of x with column rkm and distance_to_rkm of units "m".
+#' @family rkm
+#' @export
+#' @examples
+#' \dontrun{
+#' rkm <- fwa_rkm(blue_line_key = 356308001, interval = 1000)
+#' fwa_nearest_rkm(rkm[2:4,], rkm[3:5,])
+#' }
 fwa_nearest_rkm <- function(x, rkm) {
   chk::chk_s3_class(x, "sf")
   chk::chk_s3_class(rkm, "sf")
@@ -64,7 +80,7 @@ fwa_nearest_rkm <- function(x, rkm) {
   }
   blue_line_keys <- unique(x$blue_line_key)
   if(length(blue_line_keys) == 1L) {
-    return(nearest_rkm(blue_line_keys))
+    return(nearest_rkm(x, rkm))
   }
 
   x$..fwatlasbc.id <- 1:nrow(x)
--- tests/testthat/test-rkm.R
+++ tests/testthat/test-rkm.R
@@ -11,18 +11,25 @@ test_that("rkm functions work", {
 })
 
 test_that("fwa_nearest_rkm works", {
-  blk <- 356308001
   rkm <- fwa_rkm(blue_line_key = 356308001, interval = 1000)
+
   x <- rkm[rkm$rkm %in% c(0, 2, 5, 6, 7),]
   rkm <- rkm[!rkm$rkm %in% c(0, 2, 5, 6, 7),]
-  rkm$blue_line_key[rkm$rkm == 8] <- 2L
-
-  x2 <- x
-  x2$blue_line_key[1:2] <- NA_integer_
-  y <- fwa_nearest_rkm(x2, rkm)
-  x$rkm <- c(1,3,4,4,9)
+  y <- fwa_nearest_rkm(x, rkm)
   expect_is(y$distance_to_rkm, "units")
   expect_true(vld_gt(as.numeric(y$distance_to_rkm), 500))
   y$distance_to_rkm <- NULL
+  x$rkm <- c(1,3,4,4,8)
+  expect_identical(y, x)
+
+
+  rkm$blue_line_key[rkm$rkm == 8] <- 2L
+  x2 <- x
+  x2$blue_line_key[1:2] <- NA_integer_
+  y <- fwa_nearest_rkm(x2, rkm)
+  expect_is(y$distance_to_rkm, "units")
+  expect_true(vld_gt(as.numeric(y$distance_to_rkm), 500))
+  y$distance_to_rkm <- NULL
+  x$rkm <- c(1,3,4,4,9)
   expect_identical(y, x)
 })
