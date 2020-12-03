9716d3e0ab0cd0ab2e3209307b17732d5ce20d28
Joe Thorley <joe@poissonconsulting.ca>
2020-12-03 08:01:51 -0800

    - Added `fwa_add_end_id_to_rkm()`.

---
+++ R/end-rkm.R
@@ -0,0 +1,71 @@
+#' Add End ID to Rkm
+#'
+#' Adds id column to rkm based on end river kilometers in y.
+#' All rkms up to and including the end but before the previous end
+#' are assigned the positive integer id (which can be a missing value)
+#'
+#' @param rkm A data frame with columns blue_line_key and rkm.
+#' @param y A data frame with columns blue_line_key, rkm and and id column to add.
+#' @param id A string of the name of the positive integer column in y (can include missing values).
+#' @return An ordered version of rkm with additional columns from y.
+#' @export
+fwa_add_end_id_to_rkm <- function(rkm, y, id = "id") {
+  check_data(rkm, values = list(blue_line_key = c(1L, .Machine$integer.max),
+                                rkm = 1), key = c("blue_line_key", "rkm"))
+  check_data(y, values = list(blue_line_key = c(1L, .Machine$integer.max),
+                              rkm = 1), key = c("blue_line_key", "rkm"))
+
+  chk_string(id)
+  check_dim(id, nchar, TRUE)
+
+  check_data(y, values = stats::setNames(list(c(NA, 1L, .Machine$integer.max)), id))
+
+  rkm <- dplyr::as_tibble(rkm)
+  y <- dplyr::as_tibble(y)
+
+  if(!is.null(rkm[[id]])) rkm[[id]] <- NULL
+
+  rkm <- dplyr::arrange(rkm, .data$blue_line_key, .data$rkm)
+
+  if(!nrow(rkm)) {
+    rkm[[id]] <- integer(0)
+    return(rkm)
+  }
+
+  y <- y[y$blue_line_key %in% rkm$blue_line_key,,drop = FALSE]
+  if(!nrow(y)) {
+    rkm[[id]] <- NA_integer_
+    return(rkm)
+  }
+
+  y <- dplyr::arrange(y, .data$blue_line_key, .data$rkm)
+
+  y_max <- dplyr::group_by(y, .data$blue_line_key)
+  y_max <- dplyr::summarise(y_max,
+                            blue_line_key = dplyr::first(.data$blue_line_key),
+                            .fwatlasbc.y.max.. = max(.data$rkm))
+  y_max <- dplyr::ungroup(y_max)
+
+  rkm <- dplyr::left_join(rkm, y_max, by = "blue_line_key")
+  out <- is.na(rkm$.fwatlasbc.y.max..) | rkm$.fwatlasbc.y.max.. < rkm$rkm
+  rkm <- dplyr::select(rkm, -.data$.fwatlasbc.y.max..)
+  rkm_out <- dplyr::filter(rkm, out)
+  rkm <- dplyr::filter(rkm, !out)
+
+  rkm_out <- fwa_add_end_id_to_rkm(rkm_out, y[0,], id = id)
+
+  y <- dplyr::mutate(y, .fwatlasbc.y.rkm.. = .data$rkm)
+  y <- dplyr::select(y, -.data$rkm)
+
+  rkm <- dplyr::left_join(rkm, y, by = "blue_line_key")
+  rkm <- dplyr::mutate(rkm, .fwatlasbc.y.rkm.. = .data$.fwatlasbc.y.rkm.. - .data$rkm)
+  rkm <- dplyr::filter(rkm, .data$.fwatlasbc.y.rkm.. >= 0)
+  rkm <- dplyr::group_by(rkm, .data$blue_line_key, .data$rkm)
+  rkm <- dplyr::slice_min(rkm, .data$.fwatlasbc.y.rkm.., with_ties = FALSE)
+  rkm <- dplyr::ungroup(rkm)
+  rkm <- dplyr::select(rkm, -.data$.fwatlasbc.y.rkm..)
+
+  rkm <- dplyr::bind_rows(rkm, rkm_out)
+  rkm <- dplyr::arrange(rkm, .data$blue_line_key, .data$rkm)
+  rkm
+}
---
+++ tests/testthat/test-end-rkm.R
@@ -0,0 +1,52 @@
+test_that("fwa_add_end_id_to_rkm errors if no columns", {
+
+  rkm <- data.frame(blue_line_key = 1L, rkm = seq(1, 10, by = 1))
+  x <- data.frame(blue_line_key = 1L, rkm = c(3, 7.5, 9))
+
+  chk::expect_chk_error(fwa_add_end_id_to_rkm(rkm, x))
+})
+
+test_that("fwa_add_end_id_to_rkm reorders", {
+  rkm <- data.frame(blue_line_key = 1L, rkm = seq(1, 10, by = 1))
+  x <- data.frame(blue_line_key = 1L, rkm = c(3, 7.5, 9), id = 3:1)
+
+  erkm <- rkm
+  rkm <- rkm[rev(order(rkm$rkm)),]
+
+  expect_identical(dplyr::select(fwa_add_end_id_to_rkm(rkm, x), blue_line_key, rkm), dplyr::as_tibble(erkm))
+})
+
+test_that("fwa_add_end_id_to_rkm adds missing values if zero length", {
+  rkm <- data.frame(blue_line_key = 1L, rkm = seq(1, 10, by = 1))
+  x <- data.frame(blue_line_key = 1L, rkm = c(3, 7.5, 9), new = 2L)
+  rkm$new <- NA_real_
+  expect_identical(fwa_add_end_id_to_rkm(rkm, x[0,], id = "new"),
+                   dplyr::mutate(dplyr::as_tibble(rkm), new = NA_integer_))
+})
+
+test_that("fwa_add_end_id_to_rkm adds zero length", {
+  rkm <- data.frame(blue_line_key = 1L, rkm = seq(1, 10, by = 1))
+  x <- data.frame(blue_line_key = 1L, rkm = c(3, 7.5, 9), new = 2L)
+
+  erkm <- rkm[0,]
+  erkm$new <- erkm$new <- numeric(0)
+  expect_equal(fwa_add_end_id_to_rkm(rkm[0,], x, id = "new"), dplyr::as_tibble(erkm))
+})
+
+test_that("fwa_add_end_id_to_rkm simple example", {
+  rkm <- data.frame(blue_line_key = 1L, rkm = seq(1, 10, by = 1))
+  y <- data.frame(blue_line_key = 1L, rkm = c(3, 7.5, 9), new = c(3L, 7L, 10L))
+
+  erkm <- rkm
+  erkm$new <- c(3L, 3L, 3L, 7L, 7L, 7L, 7L, 10L, 10L, NA)
+  expect_identical(fwa_add_end_id_to_rkm(rkm, y, id = "new"), dplyr::as_tibble(erkm))
+})
+
+test_that("fwa_add_end_id_to_rkm with missing values", {
+  rkm <- data.frame(blue_line_key = 1L, rkm = seq(1, 10, by = 1))
+  y <- data.frame(blue_line_key = 1L, rkm = c(2, 3, 7.5, 9), id = c(NA, 3L, 7L, 10L))
+
+  erkm <- rkm
+  erkm$id <- c(NA, NA, 3L, 7L, 7L, 7L, 7L, 10L, 10L, NA)
+  expect_identical(fwa_add_end_id_to_rkm(rkm, y), dplyr::as_tibble(erkm))
+})
