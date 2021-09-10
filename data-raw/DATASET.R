50d911f62b8c52318333bb0d26ff6276e45b6ac3
Joe Thorley <joe@poissonconsulting.ca>
2021-09-10 16:11:29 -0700

    update named streams to include all

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,12 +1,22 @@
 library(dplyr)
 
-named_streams <- fwapgr::fwa_collection("whse_basemapping.fwa_named_streams")
+coll_id <- "whse_basemapping.fwa_named_streams"
 
-named_streams <- as_tibble(named_streams)
-named_streams$geometry <- NULL
+# first 10,000
+named_streams1 <- coll_id %>%
+  fwapgr::fwa_query_collection() %>%
+  as_tibble() %>%
+  select(blue_line_key, gnis_name)
 
-named_streams <- named_streams |>
-  select(blue_line_key, gnis_name) |>
+# 10,001 - 20,000
+named_streams2 <- coll_id %>%
+  fwapgr::fwa_query_collection(offset = 9999) %>%
+  slice(-1) %>%
+  as_tibble() %>%
+  select(blue_line_key, gnis_name)
+
+named_streams <- named_streams1 %>%
+  bind_rows(named_streams2) %>%
   distinct()
 
 usethis::use_data(named_streams, overwrite = TRUE, internal = TRUE)
