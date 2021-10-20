42e565074144deab31fba388ffb1f1d601690cea
Joe Thorley <joe@poissonconsulting.ca>
2021-10-20 14:25:09 -0700

    - Moved rm argument in `fwa_add_watershed_to_blk()` to column in x.

--- R/add-watershed-to-blk.R
+++ R/add-watershed-to-blk.R
@@ -8,7 +8,7 @@ adjust_watershed <- function(wshed, x, epsg) {
   }
 
   fwshed <- fwa_watershed_hex(blue_line_key = x$blk,
-                              downstream_route_measure = x$..fwa_rm,
+                              downstream_route_measure = x$rm,
                               epsg = epsg)
 
 
@@ -26,12 +26,12 @@ add_watershed_to_blk <- function(x, epsg) {
   check_dim(x, dim = nrow, values = 1L) # +chk
 
   wshed <- try(fwa_watershed_at_measure(blue_line_key = x$blk,
-                                        downstream_route_measure = x$..fwa_rm,
+                                        downstream_route_measure = x$rm,
                                         epsg = epsg), silent = TRUE)
 
   if(is_try_error(wshed)) {
     abort_chk("Unable to retrieve fundamental watershed for blk ", x$blk,
-              " at rm ", x$..fwa_rm, ".")
+              " at rm ", x$rm, ".")
   }
 
   wshed <- adjust_watershed(wshed, x, epsg)
@@ -49,9 +49,9 @@ add_watershed_to_blk <- function(x, epsg) {
 #' sf column geometry.
 #'
 #' @inheritParams fwapgr::fwa_locate_along
-#' @param x An sf object with a polygon sfc column specifying watersheds.
-#' @param rm A positive whole numeric of the distance in meters upstream
-#' from the river mouth.
+#' @param x An sf object with a polygon sfc column specifying watersheds
+#' and an optional rm column specfying the river meter.
+#' The rm is set to be 0 if missing.
 #' @param exclude A logical vector specifying whether to exclude the
 #' fundamental watershed in which the start falls.
 #' @return A sf object
@@ -62,7 +62,6 @@ add_watershed_to_blk <- function(x, epsg) {
 #' fwa_add_watershed_to_blk(data.frame(blk = 356308001))
 #' }
 fwa_add_watershed_to_blk <- function(x,
-                                     rm = 0,
                                      exclude = FALSE,
                                      epsg = getOption("fwa.epsg", 3005)) {
   check_data(x)
@@ -70,12 +69,13 @@ fwa_add_watershed_to_blk <- function(x,
   chk_whole_numeric(x$blk)
   chk_not_any_na(x$blk)
   chk_gt(x$blk)
-  chk_unique(x$blk)
+  if(!"rm" %in% names(x)) x$rm <- NA_real_
+  chk_numeric(x$rm)
+  chk_gte(x$rm)
+  x$rm[is.na(x$rm)] <- 0
+
   chk_not_subset(colnames(x), "geometry")
-  chk_not_subset(colnames(x), c("..fwa_rm", "..fwa_exclude", "..fwa_id"))
-  chk_whole_numeric(rm)
-  chk_not_any_na(rm)
-  chk_gte(rm)
+  chk_not_subset(colnames(x), c("..fwa_exclude", "..fwa_id"))
   chk_logical(exclude)
   chk_not_any_na(exclude)
   chk_whole_number(epsg)
@@ -83,12 +83,11 @@ fwa_add_watershed_to_blk <- function(x,
 
   x |>
     dplyr::as_tibble() |>
-    dplyr::mutate(..fwa_rm = rm,
-                  ..fwa_exclude = exclude,
+    dplyr::mutate(..fwa_exclude = exclude,
                   ..fwa_id = 1:dplyr::n()) |>
     dplyr::group_split(.data$blk) |>
     lapply(add_watershed_to_blk, epsg = epsg) |>
     dplyr::bind_rows() |>
     dplyr::arrange(.data$..fwa_id) |>
-    dplyr::select(-.data$..fwa_rm, -.data$..fwa_exclude, -.data$..fwa_id)
+    dplyr::select(-.data$..fwa_exclude, -.data$..fwa_id)
 }
--- tests/testthat/test-add-watershed-to-blk.R
+++ tests/testthat/test-add-watershed-to-blk.R
@@ -2,18 +2,20 @@ test_that("fwa_add_watershed_to_blk works", {
   x <- fwa_add_watershed_to_blk(data.frame(blk = 360879896L))
 
   expect_s3_class(x, "sf")
-  expect_identical(colnames(x), c("blk", "geometry"))
+  expect_identical(colnames(x), c("blk", "rm", "geometry"))
   expect_identical(x$blk, 360879896L)
+  expect_identical(x$rm, 0)
   expect_s3_class(x$geometry, "sfc_POLYGON")
   expect_equal(as.numeric(sf::st_area(x)), 154167602.934787)
 })
 
 test_that("fwa_add_watershed_to_blk", {
-  x <- fwa_add_watershed_to_blk(data.frame(blk = 360879896L), exclude = TRUE)
+  x <- fwa_add_watershed_to_blk(data.frame(blk = 360879896L, rm = 1.1), exclude = TRUE)
 
   expect_s3_class(x, "sf")
-  expect_identical(colnames(x), c("blk", "geometry"))
+  expect_identical(colnames(x), c("blk", "rm", "geometry"))
+  expect_identical(x$rm, 1.1)
   expect_identical(x$blk, 360879896L)
   expect_s3_class(x$geometry, "sfc_POLYGON")
-  expect_equal(as.numeric(sf::st_area(x)), 154121020.15179)
+  expect_equal(as.numeric(sf::st_area(x)), 154155413.126877)
 })
