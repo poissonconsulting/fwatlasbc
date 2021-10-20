100538fe04215fede57a996ff208bda36a7c37cc
Joe Thorley <joe@poissonconsulting.ca>
2021-10-20 12:20:47 -0700

    - Added `add_stream_network_to_blk()`.

--- R/add-stream-network-to-blk.R
+++ R/add-stream-network-to-blk.R
@@ -2,28 +2,32 @@ add_stream_network_to_blk <- function(x, epsg) {
   check_dim(x, dim = nrow, values = 1L) # +chk
 
   x <- x |>
-    fwa_add_watershed_to_blk(rm = x$rm, epsg = epsg) |>
+    fwa_add_watershed_to_blk(rm = x$..fwa_rm2, epsg = epsg) |>
     fwa_add_collection_to_watershed(epsg = epsg) |>
     dplyr::filter(
-      !is.na(.data$fwa_watershed_code) & stringr::str_detect(.data$fwa_watershed_code, "^999")
+      !is.na(.data$fwa_watershed_code) & !stringr::str_detect(.data$fwa_watershed_code, "^999")
     )
 
   main <- x |>
-    dplyr::filter(blue_line_key = x$blk) |>
+    dplyr::filter(.data$blue_line_key == x$blk)  |>
     dplyr::slice_max(.data$upstream_route_measure)
 
   max_rm <- main$upstream_route_measure
 
   wshed_code <- main$fwa_watershed_code |>
-    stringr::str_replace_all("-0{6,6}")
+    stringr::str_replace_all("-0{6,6}", "")
+
+  print(wshed_code)
 
   x |>
-    dplyr::filter(stringr::str_detect(.data$fwa_watershed_code == wshed_code)) |>
-    dplyr::mutate(..fwa_percent = stringr::str_replace(.data$fwa_watershed_code, wshed_code),
-                  ..fwa_percent = stringr::str_extract(.data$..fwa_percent, "^\\d{6,6}"),
-                  ..fwa_percent = as.integer(.data$..fwa_percent),
-                  ..fwa_percent = .data$..fwa_percent / 1000000) |>
-    dplyr::filter(.data$..fwa_percent >= x$rm / max_rm)
+    dplyr::filter(stringr::str_detect(.data$fwa_watershed_code, wshed_code)) |>
+    dplyr::mutate(
+      ..fwa_percent2 = stringr::str_replace(.data$fwa_watershed_code, paste0("^", wshed_code, "-"), ""),
+      ..fwa_percent2 = stringr::str_extract(.data$..fwa_percent2, "^\\d{6,6}"),
+      ..fwa_percent2 = as.integer(.data$..fwa_percent2),
+      ..fwa_percent2 = .data$..fwa_percent2 / 1000000
+    ) |>
+    dplyr::filter(.data$..fwa_percent2 >= .data$..fwa_rm2 / max_rm)
 }
 
 #' Add Stream Network to Blue Line Key
@@ -44,12 +48,12 @@ add_stream_network_to_blk <- function(x, epsg) {
 fwa_add_stream_network_to_blk <- function(x, rm = 0,
                                           epsg = getOption("fwa.epsg", 3005)) {
   x |>
-    dplyr::mutate(..fwa_id = 1:dplyr::n(),
-                  ..fwa_rm = rm,
-                  ..fwa_percent = NA_real_) |>
-    dplyr::group_split(.data$..fwa_id) |>
+    dplyr::mutate(..fwa_id2 = 1:dplyr::n(),
+                  ..fwa_rm2 = rm,
+                  ..fwa_percent2 = NA_real_) |>
+    dplyr::group_split(.data$..fwa_id2) |>
     lapply(add_stream_network_to_blk, epsg = epsg) |>
     dplyr::bind_rows() |>
-    dplyr::arrange(.data$..fwa_id) |>
-    dplyr::select(-.data$..fwa_id, -.data$..fwa_rm, -.data$..fwa_percent)
+    dplyr::arrange(.data$..fwa_id2) |>
+    dplyr::select(-.data$..fwa_id2, -.data$..fwa_rm2, -.data$..fwa_percent2)
 }
---
+++ tests/testthat/test-add-stream-network-to-blk.R
@@ -0,0 +1,21 @@
+test_that("fwa_add_stream_network_to_blk() works", {
+  x <- fwa_add_stream_network_to_blk(data.frame(blk = 356308001))
+
+  expect_s3_class(x, "sf")
+  expect_identical(sf::st_crs(x)$epsg, 3005L)
+  expect_gte(nrow(x), 82L)
+  expect_s3_class(x$geometry, "sfc_LINESTRING")
+  expect_identical(colnames(sf::st_coordinates(x)), c("X", "Y", "L1")) # why not , "Z", "L1"
+
+  expect_identical(
+    colnames(x),
+    c("blk", "id", "blue_line_key", "blue_line_key_50k", "downstream_route_measure",
+      "edge_type", "feature_code", "feature_source", "fwa_watershed_code",
+      "gnis_id", "gnis_name", "gradient", "left_right_tributary", "length_metre",
+      "linear_feature_id", "local_watershed_code", "localcode_ltree",
+      "stream_magnitude", "stream_order", "upstream_area_ha", "upstream_route_measure",
+      "waterbody_key", "watershed_code_50k", "watershed_group_code",
+      "watershed_group_code_50k", "watershed_group_id", "watershed_key",
+      "watershed_key_50k", "wscode_ltree", "geometry"))
+})
+
