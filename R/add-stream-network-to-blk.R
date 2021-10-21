8357ddb2ae4de7dc577e92f0da08c2a8caca7529
Joe Thorley <joe@poissonconsulting.ca>
2021-10-20 18:33:15 -0700

    testing add_stream_network_to_blk

--- R/add-stream-network-to-blk.R
+++ R/add-stream-network-to-blk.R
@@ -1,25 +1,36 @@
 add_stream_network_to_blk <- function(x, epsg) {
   check_dim(x, dim = nrow, values = 1L) # +chk
 
+  rm <- x$rm
+
   x <- x |>
     fwa_add_watershed_to_blk(epsg = epsg) |>
     fwa_add_collection_to_watershed(epsg = epsg) |>
     dplyr::filter(
       !is.na(.data$fwa_watershed_code) & !stringr::str_detect(.data$fwa_watershed_code, "^999")
-    )
+    ) |>
+    dplyr::as_tibble()
 
-  main <- x |>
-    dplyr::filter(.data$blue_line_key == x$blk)  |>
+  stem <- x |>
+    dplyr::filter(.data$blue_line_key == x$blk)
+
+  tip <- stem |>
     dplyr::slice_max(.data$upstream_route_measure)
 
-  max_rm <- main$upstream_route_measure
+  if(tip$upstream_route_measure < rm) {
+    stop("rm exceeds length of creek")
+  }
 
-  wshed_code <- main$fwa_watershed_code |>
+  max_rm <- tip$upstream_route_measure
+
+  wshed_code <- tip$fwa_watershed_code |>
     stringr::str_replace_all("-0{6,6}", "")
 
-  print(wshed_code)
+  root <- stem |>
+    dplyr::filter(.data$downstream_route_measure <= rm)
 
-  x |>
+  x <- x |>
+    dplyr::anti_join(root, by = "id") |>
     dplyr::filter(stringr::str_detect(.data$fwa_watershed_code, wshed_code)) |>
     dplyr::mutate(
       ..fwa_percent2 = stringr::str_replace(.data$fwa_watershed_code, paste0("^", wshed_code, "-"), ""),
@@ -28,6 +39,15 @@ add_stream_network_to_blk <- function(x, epsg) {
       ..fwa_percent2 = .data$..fwa_percent2 / 1000000
     ) |>
     dplyr::filter(.data$..fwa_percent2 >= .data$rm / max_rm)
+
+  root <- root |>
+    dplyr::filter(.data$upstream_route_measure < rm) |>
+    dplyr::slice_max(.data$downstream_route_measure) # safety against multiple
+    # need to cut segment and keep upper half
+
+  x <- x |>
+    dplyr::bind_rows(root) |>
+    sf::st_set_geometry("geometry")
 }
 
 #' Add Stream Network to Blue Line Key
