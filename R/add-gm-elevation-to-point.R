a964990e4408ac2deecea3d87b1bbd659c857b96
Joe Thorley <joe@poissonconsulting.ca>
2021-10-23 06:53:39 -0700

    - Added `chunk_size` and `digits` arguments to `fwa_add_gm_elevation_to_point()`.

--- R/add-gm-elevation-to-point.R
+++ R/add-gm-elevation-to-point.R
@@ -1,7 +1,25 @@
+add_gm_elevation_to_point <- function(x, digits, key) {
+  coords <- x |>
+    sf::st_transform(crs = 4326) |>
+    sf::st_coordinates() |>
+    dplyr::as_tibble() |>
+    dplyr::select(lon = .data$X, lat = .data$Y) |>
+    dplyr::mutate(lon = round(.data$lon, digits), lat = round(.data$lat, digits))
+
+  elevation <- googleway::google_elevation(coords, key = key)$results$elevation
+  if(is.null(elevation)) {
+    err("Invalid Google Maps Elevation API key.")
+  }
+  x$elevation <- elevation
+  x
+}
 #' Add Google Maps Elevation to Point
 #'
 #' @param x An sf object of spatial points.
-#' @param key A string of the google maps elevation api key.
+#' @param chunk_size The number of rows to include in each API query.
+#' @param digits The number of digits to round the latitude and longitude by
+#' before querying the elevation from the API
+#' @param key A string of the Google Maps Elevation API key.
 #' @return An updated version of x with numeric column elevation.
 #' @seealso `googleway::google_elevation()`
 #' @export
@@ -10,8 +28,8 @@
 #' rm <- fwa_add_rms_to_blk(data.frame(blk = 356308001))
 #' fwa_add_gm_elevation_to_point(rm)
 #' }
-fwa_add_gm_elevation_to_point <- function(x,
-                                key = Sys.getenv("GOOGLE_MAPS_ELEVATION_API_KEY")) {
+fwa_add_gm_elevation_to_point <- function(x, chunk_size = 300L, digits = 7,
+                                          key = Sys.getenv("GOOGLE_MAPS_ELEVATION_API_KEY")) {
 
   if(!requireNamespace("googleway", quietly = TRUE)) {
     err("Package 'googleway' must be installed to get elevations from Google Maps.")
@@ -19,6 +37,11 @@ fwa_add_gm_elevation_to_point <- function(x,
 
   chk_s3_class(x, "sf")
   chk_s3_class(sf::st_geometry(x), "sfc_POINT")
+  chk_not_subset(colnames(x), c("..fwa_chunk"))
+  chk_whole_numeric(chunk_size)
+  chk_gt(chunk_size)
+  chk_whole_numeric(digits)
+  chk_range(digits, c(0, 15))
   chk_string(key)
 
   if(!nrow(x)) {
@@ -26,17 +49,11 @@ fwa_add_gm_elevation_to_point <- function(x,
     return(x)
   }
 
-  coords <- x |>
-    sf::st_transform(crs = 4326) |>
-    sf::st_coordinates() |>
-    dplyr::as_tibble() |>
-    dplyr::select(lon = .data$X, lat = .data$Y)
+  x$..fwa_chunk <- 1:nrow(x) %/% chunk_size
 
-  elevation <- googleway::google_elevation(coords, key = key)$results$elevation
-  if(is.null(elevation)) {
-    err("Invalid Google Maps Elevation API key.")
-  }
-
-  x$elevation <- elevation
-  x
+  x |>
+    dplyr::group_split(.data$..fwa_chunk) |>
+    lapply(add_gm_elevation_to_point, digits = digits, key = key) |>
+     dplyr::bind_rows() |>
+     dplyr::select(-.data$..fwa_chunk)
 }
--- tests/testthat/test-add-gm-elevation-to-point.R
+++ tests/testthat/test-add-gm-elevation-to-point.R
@@ -1,13 +1,43 @@
-test_that("fwa_add_gm_elevation_to_point works", {
-  rm <- fwa_add_rms_to_blk(data.frame(blk = 356308001))
+test_that("fwa_add_gm_elevation_to_point works few", {
+  rm <- fwa_add_rms_to_blk(data.frame(blk = 356308001), interval = 1000)
 
   x <- fwa_add_gm_elevation_to_point(rm)
   expect_s3_class(x, "sf")
   expect_identical(colnames(x), c("blk", "rm", "elevation", "geometry"))
   expect_equal(x$elevation,
-                   c(5, 5, 5, 5, 5, 10.3940963745117, 5.35616397857666, 5.00019073486328,
-                     9.28616428375244, 7.15765380859375, 7.00408458709717, 8.34540557861328,
-                     10.8682670593262, 10.2141819000244, 11.2053289413452, 11.2773027420044,
-                     10.5231266021728, 75.4190673828125, 165.339157104492, 208.487533569336
-                   ))
+               c(5, 5, 5, 5, 5, 10.3936414718628, 5.35515594482422, 5.00018835067749,
+                 9.28686714172363, 7.15812873840332, 7.00370025634766, 8.34566402435303,
+                 10.8686275482178, 10.2142515182495, 11.205244064331, 11.2773637771606,
+                 10.5231037139893, 75.4199447631836, 165.338165283203, 208.487548828125
+               ))
+})
+
+test_that("fwa_add_gm_elevation_to_point works digits", {
+  rm <- fwa_add_rms_to_blk(data.frame(blk = 356308001), interval = 1000)
+
+  x <- fwa_add_gm_elevation_to_point(rm, digits = 3)
+  expect_s3_class(x, "sf")
+  expect_identical(colnames(x), c("blk", "rm", "elevation", "geometry"))
+  expect_equal(x$elevation,
+               c(5, 5, 5, 11.4579448699951, 5, 19.2013263702393, 10.6362476348877,
+                 6.682457447052, 6.72773838043213, 10.558274269104, 10.594612121582,
+                 8.23660659790039, 11.069766998291, 12.1544971466065, 11.0921401977539,
+                 11.8463001251221, 10.2729940414429, 80.8487396240234, 171.800216674805,
+                 210.517883300781))
+})
+
+test_that("fwa_add_gm_elevation_to_point works almost 1000", {
+  rm <- fwa_add_rms_to_blk(data.frame(blk = 356308001), interval = 20)
+
+  x <- fwa_add_gm_elevation_to_point(rm)
+  expect_s3_class(x, "sf")
+  expect_identical(colnames(x), c("blk", "rm", "elevation", "geometry"))
+  expect_identical(nrow(x), 962L)
+  expect_snapshot_data(x, "elevation")
+})
+
+test_that("fwa_add_gm_elevation_to_point errors with chunk size of 320", {
+  rm <- fwa_add_rms_to_blk(data.frame(blk = 356308001), interval = 20)
+
+  expect_error(fwa_add_gm_elevation_to_point(rm, chunk_size = 320))
 })
