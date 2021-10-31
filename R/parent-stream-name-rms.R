cbd28f8ca39b7fd9d9db13406532ca09a83a18d5
Joe Thorley <joe@poissonconsulting.ca>
2021-10-31 13:26:06 -0700

    - Added `fwa_parent_stream_name_rms()`.

---
+++ R/parent-stream-name-rms.R
@@ -0,0 +1,54 @@
+#' Parent Stream Name
+#'
+#' Gets parent stream name.
+#'
+#' @param x A character vector of one or more stream names.
+#' @param rms A data frame with integer columns blk and parent_blk.
+#' There must be only one parent_blk for each blk.
+#' @param stream_name A data frame with character column stream_name and integer column blk. There must be no more than one blk for the stream names specified.
+#' @return A character vector of the parent blue line keys.
+#' @export
+fwa_parent_stream_name_rms <- function(x, rms, stream_name = fwatlasbc::fwa_stream_name) {
+  chk_character_or_factor(x)
+
+  check_names(rms, c("blk", "parent_blk"))
+
+  chk_whole_numeric(rms$blk)
+  chk_not_any_na(rms$blk)
+  chk_gt(rms$blk)
+
+  chk_whole_numeric(rms$parent_blk)
+  chk_gt(rms$parent_blk)
+
+  rms <- rms |>
+    dplyr::distinct(.data$blk, .data$parent_blk)
+
+  check_key(rms, c("blk", "parent_blk"))
+
+  chk_data(stream_name)
+  check_names(stream_name, c("blk", "stream_name"))
+  chk_gt(stream_name$blk)
+  chk_whole_numeric(stream_name$blk)
+  chk_not_any_na(stream_name$blk)
+  chk_character_or_factor(stream_name$stream_name)
+  chk_not_any_na(stream_name$stream_name)
+
+  sname <- stream_name |>
+    dplyr::filter(.data$stream_name %in% x)
+
+  if(anyDuplicated(sname$stream_name)) {
+    abort_chk("Each `x` stream_name value must have one blk value in `stream_name`.")
+  }
+
+  if(!length(x)) return(character(0))
+  if(!nrow(rms)) return(rep(NA_character_, length(x)))
+
+  x <- x |>
+    dplyr::tibble(stream_name = x) |>
+    fwa_add_blks_to_stream_name(sname) |>
+    dplyr::mutate(blk = fwa_parent_blk_rms(.data$blk, rms)) |>
+    dplyr::select(-.data$stream_name) |>
+    fwa_add_stream_names_to_blk(stream_name)
+
+  x$stream_name
+}
---
+++ tests/testthat/test-parent-stream-name-rms.R
@@ -0,0 +1,48 @@
+test_that("parent_stream_name works", {
+  rms <- data.frame(blk = 10, parent_blk = 2)
+  stream_name <- data.frame(blk = c(10, 2), stream_name = c("Sub Creek", "Super Creek"))
+  expect_identical(fwa_parent_stream_name_rms("Sub Creek", rms, stream_name), "Super Creek")
+})
+
+test_that("parent_stream_name works no length", {
+  rms <- data.frame(blk = 10, parent_blk = 2)
+  stream_name <- data.frame(blk = c(10, 2), stream_name = c("Sub Creek", "Super Creek"))
+  expect_identical(fwa_parent_stream_name_rms(character(0), rms, stream_name), character(0))
+})
+
+test_that("parent_stream_name works missing values", {
+  rms <- data.frame(blk = 10, parent_blk = 2)
+  stream_name <- data.frame(blk = c(10, 2), stream_name = c("Sub Creek", "Super Creek"))
+  expect_identical(fwa_parent_stream_name_rms(NA_character_, rms, stream_name), NA_character_)
+})
+
+test_that("parent_stream_name works multiple", {
+  rms <- data.frame(blk = 10, parent_blk = 2)
+  stream_name <- data.frame(blk = c(10, 2), stream_name = c("Sub Creek", "Super Creek"))
+  expect_identical(fwa_parent_stream_name_rms(c("Sub Creek", "Sub Creek"), rms, stream_name), c("Super Creek", "Super Creek"))
+})
+
+test_that("parent_stream_name works absent", {
+  rms <- data.frame(blk = 10, parent_blk = 2)
+  stream_name <- data.frame(blk = c(10, 2), stream_name = c("Sub Creek", "Super Creek"))
+  expect_identical(fwa_parent_stream_name_rms("Not a creek", rms, stream_name), NA_character_)
+})
+
+test_that("parent_stream_name works if unrooted", {
+  rms <- data.frame(blk = 10, parent_blk = 2)
+  stream_name <- data.frame(blk = c(10, 2), stream_name = c("Sub Creek", "Super Creek"))
+  expect_identical(fwa_parent_stream_name_rms("Super Creek", rms, stream_name), NA_character_)
+})
+
+test_that("parent_stream_name errors if multiple in child stream name", {
+  rms <- data.frame(blk = 10, parent_blk = 2)
+  stream_name <- data.frame(blk = c(10, 11, 2), stream_name = c("Sub Creek", "Sub Creek", "Super Creek"))
+ chk::expect_chk_error(fwa_parent_stream_name_rms("Sub Creek", rms, stream_name), NA_character_)
+})
+
+test_that("parent_stream_name handles multiple in parent stream name", {
+  rms <- data.frame(blk = 10, parent_blk = 2)
+  stream_name <- data.frame(blk = c(10, 11, 2), stream_name = c("Sub Creek", "Super Creek", "Super Creek"))
+  expect_identical(fwa_parent_stream_name_rms("Sub Creek", rms, stream_name), "Super Creek")
+})
+
