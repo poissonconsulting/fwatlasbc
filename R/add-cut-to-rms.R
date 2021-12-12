46a46544f727905e3abd56d8d97c266d09d40d1e
Joe Thorley <joe@poissonconsulting.ca>
2021-12-12 13:55:34 -0800

    - Added `fwa_add_cut_rm()`.

---
+++ R/add-cut-to-rms.R
@@ -0,0 +1,66 @@
+#' Add Cut to River Meter
+#'
+#' Adds value(s) in cut column in y to to column of same name in x based on blk and rm_start and rm_end in y.
+#'
+#' @param x A data frame with integer columns blk and rm.
+#' @param y A data frame with integer columns blk, rm_start, rm_end and column specified in cut.
+#' @param cut A string of the name of the column in y with the values.
+#' @return A tibble of x with cut column from y.
+#' @export
+fwa_add_cut_to_rms <- function(x, y, cut = "cut") {
+  chk_data(x)
+  chk_data(y)
+  chk_string(cut)
+  chk_not_subset(cut, c("blk", "rm", "rm_start", "rm_end"))
+  check_names(x, c("blk", "rm"))
+  check_names(y, c("blk", "rm_start", "rm_end", cut))
+  chk_not_subset(colnames(x), cut)
+
+  chk_whole_numeric(x$blk)
+  chk_not_any_na(x$blk)
+  chk_gt(x$blk)
+  chk_whole_numeric(x$rm)
+  chk_not_any_na(x$rm)
+  chk_gte(x$rm)
+  chk_whole_numeric(x$blk)
+
+  chk_whole_numeric(y$blk)
+  chk_not_any_na(y$blk)
+  chk_gt(y$blk)
+  chk_whole_numeric(y$rm_start)
+  chk_not_any_na(y$rm_start)
+  chk_gte(y$rm_start)
+  chk_whole_numeric(y$rm_end)
+  chk_not_any_na(y$rm_end)
+  chk_gte(y$rm_end)
+
+  sf_column_name <- sf_column_name(x)
+
+  x <- x |>
+    dplyr::as_tibble()
+
+  if(!nrow(x)) {
+    x[[cut]] <- y[[cut]][0]
+    return(x)
+  }
+
+  y <- y |>
+    dplyr::as_tibble() |>
+    dplyr::filter(.data$blk %in% x$blk)
+
+  x[[cut]] <- y[[cut]][0][1] # gets missing value
+  if(!nrow(y)) {
+    return(x)
+  }
+
+  for(i in seq_len(nrow(y))) {
+    min_rm <- min(y$rm_start[i], y$rm_end[i])
+    max_rm <- max(y$rm_start[i], y$rm_end[i])
+    x[[cut]][x$blk == y$blk[i] & x$rm >= min_rm & x$rm <= max_rm] <-  y[[cut]][i]
+  }
+  if(!identical(sf_column_name, character(0))) {
+    x <- x |>
+      sf::st_sf(sf_column_name = sf_column_name)
+  }
+  x
+}
---
+++ tests/testthat/test-add-cut-to-rms.R
@@ -0,0 +1,144 @@
+test_that("fwa_add_cut_to_rms simple example", {
+  x <- data.frame(blk = 1L, rm = seq(1000L, 10000L, by = 1000L))
+  y <- data.frame(blk = 1L,
+                  rm_start = c(3000L, 7500L, 9000L),
+                  rm_end = c(4000L, 8500L, 100000L), cut = c("3", "7", "10"))
+
+  x <- fwa_add_cut_to_rms(x, y)
+  expect_s3_class(x, "tbl")
+  expect_identical(colnames(x), c("blk", "rm", "cut"))
+  expect_identical(x$blk, rep(1L, 10))
+  expect_identical(x$rm, seq(1000L, 10000L, by = 1000L))
+  expect_identical(x$cut, c(NA, NA, "3", "3", NA, NA, NA, "7", "10", "10"))
+})
+
+test_that("fwa_add_cut_to_rms preserves order", {
+  x <- data.frame(blk = 1L, rm = rev(seq(1000L, 10000L, by = 1000L)))
+  y <- data.frame(blk = 1L,
+                  rm_start = c(3000L, 7500L, 9000L),
+                  rm_end = c(4000L, 8500L, 100000L), cut = c("3", "7", "10"))
+
+  x <- fwa_add_cut_to_rms(x, y)
+  expect_s3_class(x, "tbl")
+  expect_identical(colnames(x), c("blk", "rm", "cut"))
+  expect_identical(x$blk, rep(1L, 10))
+  expect_identical(x$rm, rev(seq(1000L, 10000L, by = 1000L)))
+  expect_identical(x$cut, c("10", "10", "7", NA, NA, NA, "3", "3", NA, NA))
+})
+
+test_that("fwa_add_cut_to_rms adds missing values if zero length", {
+  x <- data.frame(blk = 1L, rm = seq(1000L, 10000L, by = 1000L))
+  y <- data.frame(blk = 1L,
+                  rm_start = c(3000L, 7500L, 9000L),
+                  rm_end = c(4000L, 8500L, 100000L), new = c("3", "7", "10"))
+
+  y$new <- NA_real_
+  x <- fwa_add_cut_to_rms(x, y[0,], cut = "new")
+  expect_s3_class(x, "tbl")
+  expect_identical(colnames(x), c("blk", "rm", "new"))
+  expect_identical(x$blk, rep(1L, 10))
+  expect_identical(x$rm, seq(1000L, 10000L, by = 1000L))
+  expect_identical(x$new, rep(NA_real_, 10))
+})
+
+test_that("fwa_add_cut_to_rms adds zero length", {
+  x <- data.frame(blk = 1L, rm = seq(1000L, 10000L, by = 1000L))
+  y <- data.frame(blk = 1L,
+                  rm_start = c(3000L, 7500L, 9000L),
+                  rm_end = c(4000L, 8500L, 100000L), new = c("3", "7", "10"))
+  x <- x[0,]
+  y <- y[0,]
+  y$cut <- character(0)
+  x <- fwa_add_cut_to_rms(x, y)
+  expect_s3_class(x, "tbl")
+  expect_identical(colnames(x), c("blk", "rm", "cut"))
+  expect_identical(x$blk, integer(0))
+  expect_identical(x$rm, integer(0))
+  expect_identical(x$cut, character(0))
+})
+
+test_that("fwa_add_cut_to_rms handles missing section values", {
+  x <- data.frame(blk = 1L, rm = seq(1000L, 10000L, by = 1000L))
+  y <- data.frame(blk = 1L,
+                  rm_start = c(3000L, 7500L, 9000L),
+                  rm_end = c(4000L, 8500L, 100000L), cut = c(NA, "7", "10"))
+
+  x <- fwa_add_cut_to_rms(x, y)
+  expect_s3_class(x, "tbl")
+  expect_identical(colnames(x), c("blk", "rm", "cut"))
+  expect_identical(x$blk, rep(1L, 10))
+  expect_identical(x$rm, seq(1000L, 10000L, by = 1000L))
+  expect_identical(x$cut, c(NA, NA, NA, NA, NA, NA, NA, "7", "10", "10"))
+})
+
+test_that("fwa_add_cut_to_rms doesn't grab extra column", {
+  x <- data.frame(blk = 1L, rm = seq(1000L, 10000L, by = 1000L))
+  y <- data.frame(blk = 1L,
+                  rm_start = c(3000L, 7500L, 9000L),
+                  rm_end = c(4000L, 8500L, 100000L), cut = c("3", "7", "10"),
+                  extra = 1:3)
+
+  x <- fwa_add_cut_to_rms(x, y)
+  expect_s3_class(x, "tbl")
+  expect_identical(colnames(x), c("blk", "rm", "cut"))
+  expect_identical(x$blk, rep(1L, 10))
+  expect_identical(x$rm, seq(1000L, 10000L, by = 1000L))
+  expect_identical(x$cut, c(NA, NA, "3", "3", NA, NA, NA, "7", "10", "10"))
+})
+
+test_that("fwa_add_cut_to_rms sf", {
+  x <- dplyr::tribble(
+    ~blk, ~rm, ~parent_blk, ~parent_rm,  ~x, ~y, ~popn,
+    1,     0,    NA_integer_,   NA_integer_, 0, 0, "p1",
+    1,      1,    NA_integer_,   NA_integer_, 1, 0, "p2",
+    1,      2,    NA_integer_,   NA_integer_, 2, 0, "p3",
+    1,      3,    NA_integer_,   NA_integer_, 3, 0, "p4",
+    2,     0,    1,             0.5,          0.5, 0, "pa",
+    2,     1,    1,             0.5,          0.5, 1, "pb",
+    2,     2,    1,             0.5,          0.5, 2, "pc") |>
+    sf::st_as_sf(coords = c("x", "y"), dim = "XY")
+
+  y <- data.frame(blk = 1L,
+                  rm_start = c(1L, 3L),
+                  rm_end = c(2L, 10L),
+                  cut = c(TRUE, FALSE))
+
+
+  x <- fwa_add_cut_to_rms(x,y)
+  expect_s3_class(x, "sf")
+  expect_identical(colnames(x), c("blk", "rm", "parent_blk", "parent_rm", "popn", "geometry",
+                                  "cut"))
+  expect_identical(nrow(x), 7L)
+  expect_identical(x$cut, c(NA, TRUE, TRUE, FALSE, NA, NA, NA))
+
+  expect_snapshot_data(x, "addsectionsf")
+})
+
+test_that("fwa_add_cut_to_rms sf preserve geometry2", {
+  x <- dplyr::tribble(
+    ~blk, ~rm, ~parent_blk, ~parent_rm,  ~x, ~y, ~popn,
+    1,     0,    NA_integer_,   NA_integer_, 0, 0, "p1",
+    1,      1,    NA_integer_,   NA_integer_, 1, 0, "p2",
+    1,      2,    NA_integer_,   NA_integer_, 2, 0, "p3",
+    1,      3,    NA_integer_,   NA_integer_, 3, 0, "p4",
+    2,     0,    1,             0.5,          0.5, 0, "pa",
+    2,     1,    1,             0.5,          0.5, 1, "pb",
+    2,     2,    1,             0.5,          0.5, 2, "pc") |>
+    sf::st_as_sf(coords = c("x", "y"), dim = "XY") |>
+    dplyr::mutate(geometry2 = geometry) |>
+    sf::st_set_geometry("geometry2") |>
+    sf::st_set_crs(4326)
+
+  y <- data.frame(blk = 1L,
+                  rm_start = c(1L, 3L),
+                  rm_end = c(2L, 10L),
+                  cut = c(TRUE, FALSE))
+
+  x <- fwa_add_cut_to_rms(x,y)
+  expect_s3_class(x, "sf")
+  expect_identical(colnames(x), c("blk", "rm", "parent_blk", "parent_rm", "popn", "geometry", "geometry2",
+                                  "cut"))
+  expect_identical(nrow(x), 7L)
+  expect_identical(x$cut, c(NA, TRUE, TRUE, FALSE, NA, NA, NA))
+  expect_identical(sf::st_geometry(x), x$geometry2)
+})
