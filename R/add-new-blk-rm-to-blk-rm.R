d8ed4ef9b5c47d424745f11170f0c85273915e91
Joe Thorley <joe@poissonconsulting.ca>
2023-01-14 18:36:14 -0800

    add new blk to blk rm rename

--- R/add-new-blk-rm-to-blk-rm.R
+++ R/add-new-blk-rm-to-blk-rm.R
@@ -1,26 +1,34 @@
 #' Add New Blue Line Key and River Meter to Blue Line Key and River Meter
 #'
-#' Adds new blk and rm values where existing blk and rm values
-#' based on look up in y.
+#' Adds new blk and rm values to existing blk and rm values
+#' based on look up in second data frame.
 #'
 #' @param x A tibble (or sf object) with blk and rm columns.
 #' @param y A tibble (or sf object) with unique blk and rm columns
-#' and new blk and rm columns.
+#' and new blk (optional) and rm columns.
 #' @param blk A string of the name of the blk column in x.
 #' @param rm A string of the name of the rm column in x.
 #' @param blk2 A string of the name of the blk column in y.
 #' @param rm2 A string of the name of the rm column in y.
-#' @param new_blk A string of the name of the new blk column in y.
+#' @param new_blk An optional string of the name of the new blk column in y.
+#' If NULL it is not used in the join.
 #' @param new_rm A string of the name of the new rm column in y.
+#' @param new_blk_to A string specifying the name to rename
+#' the new blk column with.
+#' @param new_rm_to A string specifying the name to rename
+#' the new blk column with.
 #' @return An updated version of x with
 #' additional columns for the new blk and rm columns.
+#' @seealso `[fwa_add_new_rm_to_blk_rm()]`
 #' @export
 fwa_add_new_blk_rm_to_blk_rm <- function(
     x, y,
     blk = "blk", rm = "rm",
     blk2 = "blk", rm2 = "rm",
     new_blk = "new_blk",
-    new_rm = "new_rm") {
+    new_rm = "new_rm",
+    new_blk_to = new_blk,
+    new_rm_to = new_rm) {
 
   chk_data(x)
   chk_data(y)
@@ -29,15 +37,22 @@ fwa_add_new_blk_rm_to_blk_rm <- function(
   chk_string(rm)
   chk_string(blk2)
   chk_string(rm2)
-  chk_string(new_blk)
+  chk_null_or(new_blk, vld = vld_string)
   chk_string(new_rm)
-  chk_function(rename)
+  chk_null_or(new_blk_to, vld = vld_string)
+  chk_string(new_rm_to)
+
+  if(vld_string(new_blk) && new_blk == blk2) {
+    new_blk <- NULL
+    new_blk_to <- NULL
+  }
 
   chk_unique(c(blk, rm, new_blk, new_rm))
+  chk_unique(c(blk, rm, new_blk_to, new_rm_to))
   chk_unique(c(blk2, rm2, new_blk, new_rm))
 
   check_names(x, c(blk, rm))
-  chk_not_subset(colnames(x), c(new_blk, new_rm))
+  chk_not_subset(colnames(x), c(new_blk_to, new_rm_to))
   check_names(y, c(blk2, rm2, new_blk, new_rm))
 
   chk_whole_numeric(x[[blk]])
@@ -56,8 +71,10 @@ fwa_add_new_blk_rm_to_blk_rm <- function(
 
   check_key(y, c(blk2, rm2))
 
-  chk_whole_numeric(y[[new_blk]])
-  chk_gt(y[[new_blk]])
+  if(!is.null(new_blk)) {
+    chk_whole_numeric(y[[new_blk]])
+    chk_gt(y[[new_blk]])
+  }
 
   chk_whole_numeric(y[[new_rm]])
   chk_gte(y[[new_rm]])
@@ -65,10 +82,12 @@ fwa_add_new_blk_rm_to_blk_rm <- function(
   y <- dplyr::as_tibble(y) |>
     dplyr::select(all_of(c(blk2, rm2, new_blk, new_rm)))
 
+  colnames(y) <- c(blk2, rm2, new_blk_to, new_rm_to)
+
   join <- c(blk = blk2, rm = rm2)
   names(join) <- c(blk, rm)
 
-  x <- x |>
+  x |>
     dplyr::left_join(y, by = join) |>
-    dplyr::relocate(all_of(c(new_blk, new_rm)), .after = all_of(rm))
+    dplyr::relocate(all_of(c(new_blk_to, new_rm_to)), .after = all_of(rm))
 }
--- tests/testthat/test-add-new-blk-rm-to-blk-rm.R
+++ tests/testthat/test-add-new-blk-rm-to-blk-rm.R
@@ -54,7 +54,7 @@ test_that("fwa_add_new_blk_to_blk_rm works tibble and missing values", {
   expect_equal(x$new_rm, c(0, NA, 5000, 6000, 7000))
 })
 
-test_that("fwa_add_new_blk_to_blk_rm works tibble and missing values", {
+test_that("fwa_add_new_blk_to_blk_rm works different names", {
   rlang::local_options(nocache = TRUE)
 
   rm <- fwa_add_rms_to_blk(data.frame(blk = 356308001))
@@ -86,4 +86,92 @@ test_that("fwa_add_new_blk_to_blk_rm works tibble and missing values", {
   expect_equal(x$old_blk, c(356308001, NA, rep(356308001, 3)))
   expect_equal(x$rm1, c(0, 2000, 5000, 6000, 7000))
   expect_equal(x$old_rm, c(0, NA, 5000, 6000, 7000))
+})
+
+
+test_that("fwa_add_new_blk_to_blk_rm works same blk2", {
+  rlang::local_options(nocache = TRUE)
+
+  rm <- fwa_add_rms_to_blk(data.frame(blk = 356308001))
+
+  x <- rm[rm$rm %in% c(0, 2000, 5000, 6000, 7000),]
+  rm <- rm[rm$rm %in% c(0, 2000, 5000, 6000, 7000, 10000),]
+  x <- fwa_snap_rms_to_rms(x, rm)
+  rm <- x$rm
+  x <- x$x
+
+  rm <- x |>
+    dplyr::slice(-2)
+
+  x <- x |>
+    dplyr::as_tibble() |>
+    dplyr::select(blk, rm, elevation)
+
+
+  x <- fwa_add_new_blk_rm_to_blk_rm(x, rm, new_blk = "blk")
+
+  expect_s3_class(x, "tbl_df")
+  expect_identical(colnames(x), c("blk", "rm", "new_rm", "elevation"))
+  expect_equal(x$blk, rep(356308001, 5))
+  expect_equal(x$rm, c(0, 2000, 5000, 6000, 7000))
+  expect_equal(x$new_rm, c(0, NA, 5000, 6000, 7000))
+})
+
+test_that("fwa_add_new_blk_to_blk_rm works new_blk = NULL", {
+  rlang::local_options(nocache = TRUE)
+
+  rm <- fwa_add_rms_to_blk(data.frame(blk = 356308001))
+
+  x <- rm[rm$rm %in% c(0, 2000, 5000, 6000, 7000),]
+  rm <- rm[rm$rm %in% c(0, 2000, 5000, 6000, 7000, 10000),]
+  x <- fwa_snap_rms_to_rms(x, rm)
+  rm <- x$rm
+  x <- x$x
+
+  rm <- x |>
+    dplyr::slice(-2)
+
+  x <- x |>
+    dplyr::as_tibble() |>
+    dplyr::select(blk, rm, elevation)
+
+
+  x <- fwa_add_new_blk_rm_to_blk_rm(x, rm, new_blk = NULL)
+
+  expect_s3_class(x, "tbl_df")
+  expect_identical(colnames(x), c("blk", "rm", "new_rm", "elevation"))
+  expect_equal(x$blk, rep(356308001, 5))
+  expect_equal(x$rm, c(0, 2000, 5000, 6000, 7000))
+  expect_equal(x$new_rm, c(0, NA, 5000, 6000, 7000))
+})
+
+test_that("fwa_add_new_blk_to_blk_rm works rename", {
+  rlang::local_options(nocache = TRUE)
+
+  rm <- fwa_add_rms_to_blk(data.frame(blk = 356308001))
+
+  x <- rm[rm$rm %in% c(0, 2000, 5000, 6000, 7000),]
+  rm <- rm[rm$rm %in% c(0, 2000, 5000, 6000, 7000, 10000),]
+  x <- fwa_snap_rms_to_rms(x, rm)
+  rm <- x$rm
+  x <- x$x
+
+  rm <- x |>
+    dplyr::slice(-2) |>
+    mutate(new_blk = new_blk + 1L)
+
+  x <- x |>
+    dplyr::as_tibble() |>
+    dplyr::select(blk, rm, elevation)
+
+
+  x <- fwa_add_new_blk_rm_to_blk_rm(x, rm, new_blk_to = "zz_blk",
+                                    new_rm_to = "aa")
+
+  expect_s3_class(x, "tbl_df")
+  expect_identical(colnames(x), c("blk", "rm", "zz_blk", "aa", "elevation"))
+  expect_equal(x$blk, rep(356308001, 5))
+  expect_equal(x$rm, c(0, 2000, 5000, 6000, 7000))
+  expect_equal(x$zz_blk, c(356308002L, NA, rep(356308002L, 3)))
+  expect_equal(x$aa, c(0, NA, 5000, 6000, 7000))
 })
