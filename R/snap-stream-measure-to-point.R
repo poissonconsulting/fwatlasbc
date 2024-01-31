1bc7f63ce9cea8b60aaf810833779568785efb74
Joe Thorley <joe@poissonconsulting.ca>
2024-01-31 09:14:55 -0800

    getting stream name

--- R/snap-stream-measure-to-point.R
+++ R/snap-stream-measure-to-point.R
@@ -1,3 +1,52 @@
+nearest_stream <- function(x, streams) {
+  index <- sf::st_nearest_feature(x, streams)
+  streams <- streams[index,]
+  x$..fwa_blk <- streams$blk
+  x$distance_to_stream <- sf::st_distance(x, streams, by_element = TRUE)
+  x$distance_to_stream <- as.numeric(x$distance_to_stream)
+  x
+}
+
+adjust_streams <- function(streams, x) {
+  if(ncol(streams) == 2) {
+    return(streams)
+  }
+
+  cols <- streams |>
+    colnames() |>
+    setdiff(c("blk", "geometry"))
+
+  cols <- x |>
+    as_tibble() |>
+    dplyr::select(tidyselect::vars_select_helpers$all_of(cols)) |>
+    dplyr::slice(1L) |>
+    dplyr::select(tidyselect::vars_select_helpers$where(function(x) !is.na(x[1]))) |>
+    colnames()
+
+  if(!length(cols)) {
+    return(streams)
+  }
+  streams |>
+    dplyr::semi_join(as_tibble(x), by = cols)
+}
+
+snap_stream_measure_to_point <- function(x, streams) {
+  if(!is.na(x$blk[1])) {
+    streams <- streams[streams$blk == x$blk[1],]
+  }
+  print(streams)
+  streams <- adjust_streams(streams, x)
+  print(streams)
+
+  if(!nrow(streams)) {
+    x$stream_measure <- NA_real_
+    x$distance_to_stream <- NA_real_
+    return(x)
+  }
+
+  nearest_stream(x, streams)
+}
+
 #' Snap Stream Measure to Point
 #'
 #' Assigns closest stream measure in m to each spatial point.
@@ -30,12 +79,12 @@ fwa_snap_stream_measure_to_point <- function(x, streams, ...) {
 
   if(!nrow(x)) {
     x$stream_measure <- double(0)
-    x$distance_to_rm <- numeric(0)
+    x$distance_to_stream <- numeric(0)
     return(x)
   }
   if(!nrow(streams)) {
     x$stream_measure <- NA_real_
-    x$distance_to_rm <- NA_real_
+    x$distance_to_stream <- NA_real_
     return(x)
   }
 
@@ -50,14 +99,14 @@ fwa_snap_stream_measure_to_point <- function(x, streams, ...) {
   x |>
     dplyr::mutate(..fwa_id = 1:dplyr::n()) |>
     group_split_sf(.data$blk, ...) |>
-  #   lapply(snap_rm_to_point, rm = rm) |>
-  #   dplyr::bind_rows() |>
-  #   dplyr::arrange(.data$..fwa_id) |>
-  #   dplyr::mutate(
-  #     ..fwa_blk = as.integer(.data$..fwa_blk),
-  #     blk = as.integer(.data$blk),
-  #     blk = dplyr::if_else(is.na(.data$..fwa_blk), .data$blk, .data$..fwa_blk)) |>
-  #   dplyr::relocate("distance_to_rm", .after = "rm") |>
-  #   dplyr::select(!c("..fwa_id", "..fwa_blk")) |>
+     lapply(snap_stream_measure_to_point, streams = streams) |>
+    dplyr::bind_rows() |>
+    dplyr::arrange(.data$..fwa_id) |>
+    dplyr::mutate(
+      ..fwa_blk = as.integer(.data$..fwa_blk),
+      blk = as.integer(.data$blk),
+      blk = dplyr::if_else(is.na(.data$..fwa_blk), .data$blk, .data$..fwa_blk)) |>
+  #   dplyr::relocate("distance_to_stream", .after = "rm") |>
+     dplyr::select(!c("..fwa_id", "..fwa_blk")) |>
   identity()
 }
--- tests/testthat/test-snap-stream-measure-to-point.R
+++ tests/testthat/test-snap-stream-measure-to-point.R
@@ -7,21 +7,42 @@ test_that("fwa_snap_stream_measure_to_point", {
   points <- fwa_convert_streams_to_rms(streams, interval = 500) |>
     dplyr::select(blk, old_rm = rm)
 
-
   pointsFT <- fwa_snap_stream_measure_to_point(points[FALSE,], streams)
+  expect_identical(nrow(pointsFT), 0L)
   expect_snapshot_data(pointsFT, "pointsFT")
   pointsTF <- fwa_snap_stream_measure_to_point(points, streams[FALSE,])
-  expect_snapshot_data(pointsFT, "pointsTF")
+  expect_identical(nrow(pointsTF), nrow(points))
+  expect_snapshot_data(pointsTF, "pointsTF")
   pointsFF <- fwa_snap_stream_measure_to_point(points[FALSE,], streams[FALSE,])
-  expect_snapshot_data(pointsFT, "pointsFF")
+  expect_identical(nrow(pointsFF), 0L)
+  expect_snapshot_data(pointsFF, "pointsFF")
+
+  points$extra <- 1:nrow(points)
 
   points_no_blk <- points |> dplyr::select(!blk)
   pointsFT_no_blk <- fwa_snap_stream_measure_to_point(points_no_blk[FALSE,], streams)
-  expect_snapshot_data(pointsFT, "pointsFT_no_blk")
+  expect_identical(nrow(pointsFT_no_blk), 0L)
+  expect_snapshot_data(pointsFT_no_blk, "pointsFT_no_blk")
   pointsTF_no_blk <- fwa_snap_stream_measure_to_point(points_no_blk, streams[FALSE,])
-  expect_snapshot_data(pointsFT, "pointsTF_no_blk")
+  expect_identical(nrow(pointsTF_no_blk), nrow(points))
+  expect_snapshot_data(pointsTF_no_blk, "pointsTF_no_blk")
   pointsFF_no_blk <- fwa_snap_stream_measure_to_point(points_no_blk[FALSE,], streams[FALSE,])
-  expect_snapshot_data(pointsFT, "pointsFF_no_blk")
+  expect_identical(nrow(pointsFF_no_blk), 0L)
+  expect_snapshot_data(pointsFF_no_blk, "pointsFF_no_blk")
 
-  new <- fwa_snap_stream_measure_to_point(points, streams)
+  stream_measure <- fwa_snap_stream_measure_to_point(points, streams)
+  expect_identical(nrow(stream_measure), nrow(points))
+  expect_snapshot_data(stream_measure, "stream_measure")
+
+  stream_measure_no_blk <- fwa_snap_stream_measure_to_point(points_no_blk, streams)
+  expect_identical(nrow(stream_measure_no_blk), nrow(points))
+  expect_snapshot_data(stream_measure_no_blk, "stream_measure_no_blk")
+  expect_identical(stream_measure_no_blk$blk, stream_measure$blk)
+
+  points_no_blk_off <- points_no_blk
+  points_no_blk_off$geometry <- st_set_crs(points_no_blk_off$geometry - 1000, st_crs(points))
+
+  stream_measure_no_blk_off <- fwa_snap_stream_measure_to_point(points_no_blk_off, streams)
+  expect_identical(nrow(stream_measure_no_blk_off), nrow(points))
+  expect_snapshot_data(stream_measure_no_blk_off, "stream_measure_no_blk_off")
 })
