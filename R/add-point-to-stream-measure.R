ebadfe882503f9ee1dd4a81da0c5dc1b111e1195
Joe Thorley <joe@poissonconsulting.ca>
2024-10-30 09:41:11 -0700

    Update R/add-point-to-stream-measure.R
    Co-authored-by: Duncan Kennedy <duncan@poissonconsulting.ca>

--- R/add-point-to-stream-measure.R
+++ R/add-point-to-stream-measure.R
@@ -10,7 +10,7 @@ add_point_to_stream_measure <- function(x, streams) {
 #' Add Point to Stream Measure
 #'
 #' Adds geometry for stream measure in m based on the blue line key (blk)
-#' as well as the proportion of the stream measure along the stream (proportion).
+#' and the proportion of the stream measure along the stream.
 #' If proportion is >= 1 then the geometry is the top of the stream.
 #'
 #' @param x An data frame with columns blk and stream_measure.
