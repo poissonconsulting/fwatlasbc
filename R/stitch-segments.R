d947cad8c5ddefb753f207c728fd376f42f915e5
Ayla Pearson <ayla@poissonconsulting.ca>
2025-12-11 14:50:44 -0800

    fixing r cmd check errors and docs

--- R/stitch-segments.R
+++ R/stitch-segments.R
@@ -4,7 +4,10 @@
 #'
 #' @param x A multiline string for a stream.
 #' @param ... Unused.
-#' @param tolerance
+#' @param tolerance A numeric value for the maximum euclidean distance between
+#'   the end a segment to the start of a segment that will create a stitched
+#'   segment. If the distance is greater then the tolerance then a segment will
+#'   not be created.
 #'
 #' @returns A data frame with the stitched segments added into the geometries.
 #' @export
@@ -13,12 +16,35 @@
 #' MULTILINESTRING are in order and the correct direction to be stitched
 #' together.
 #'
-#' If the segments could not be joined they will be returned as a MULTILINESTRING,
-#' if the segments have no gaps left the row will be returned as a LINESTRING.
-#'
-#'
+#' If the segments could not be joined they will be returned as a
+#' MULTILINESTRING, if the segments have no gaps left the row will be returned
+#' as a LINESTRING.
 #'
 #' @examples
+#' \dontrun{
+#' test_stream <- sf::st_sfc(
+#'   sf::st_multilinestring(
+#'     c(sf::st_linestring(matrix(c(0, 0, 1, 1), ncol = 2, byrow = TRUE)),
+#'       sf::st_linestring(matrix(c(1.05, 1.05, 2, 2), ncol = 2, byrow = TRUE)),
+#'       sf::st_linestring(matrix(c(2.1, 2.1, 3, 3), ncol = 2, byrow = TRUE))
+#'     )
+#'   ),
+#'   crs = 26911
+#'  )
+#'
+#'  df <- data.frame(
+#'   name = "stream 1",
+#'   blk = 1,
+#'   geometry = test_stream
+#'  ) |>
+#'  sf::st_set_geometry("geometry")
+#'
+#'  # to create two 2 segments to connect the whole line
+#'  fwa_stitch_segments(df)
+#'
+#'  # tolerance is set so only 1 segment is added and the line is still not connected
+#'  fwa_stitch_segments(df, tolerance = 0.1)
+#' }
 fwa_stitch_segments <- function(x, ..., tolerance = 5) {
   chk_s3_class(x, "data.frame")
   chk_s3_class(x, "sf")
@@ -43,7 +69,7 @@ fwa_stitch_segments <- function(x, ..., tolerance = 5) {
     x |>
     sf::st_zm() |>
     sf::st_sf() |>
-    dplyr::rename(geometry := !!sf_column_name) |>
+    dplyr::rename("geometry" := !!sf_column_name) |>
     dplyr::rowwise() |>
     dplyr::group_split()
 
@@ -58,7 +84,7 @@ fwa_stitch_segments <- function(x, ..., tolerance = 5) {
       next
     }
 
-    segments <- st_cast(split_df[[i]][["geometry"]], "LINESTRING")
+    segments <- sf::st_cast(split_df[[i]][["geometry"]], "LINESTRING")
     df_distances <- segment_end_to_start_distance(segments)
 
     new_segments <- list()
@@ -68,23 +94,23 @@ fwa_stitch_segments <- function(x, ..., tolerance = 5) {
         next
       }
 
-      end <- st_line_sample(segments[df_distances[j,]$end], sample = 1)
-      start <- st_line_sample(segments[df_distances[j,]$start], sample = 0)
+      end <- sf::st_line_sample(segments[df_distances[j,]$end], sample = 1)
+      start <- sf::st_line_sample(segments[df_distances[j,]$start], sample = 0)
 
-      line <- st_linestring(rbind(st_coordinates(start), st_coordinates(end)))
+      line <- sf::st_linestring(rbind(sf::st_coordinates(start), sf::st_coordinates(end)))
       new_segments <- c(new_segments, list(line))
     }
 
-    new_sf <- st_sfc(new_segments, crs = st_crs(x))
-    new_sf <- st_sf(geometry = new_sf) |>
-      st_zm()
+    new_sf <- sf::st_sfc(new_segments, crs = sf::st_crs(x))
+    new_sf <- sf::st_sf(geometry = new_sf) |>
+      sf::st_zm()
 
     all_segments <- dplyr::bind_rows(split_df[[i]], new_sf) |>
-      st_sf()
+      sf::st_sf()
 
-    multi <- st_combine(all_segments)
-    multi <- st_cast(multi, "MULTILINESTRING")
-    multi_sf <- st_sf(geometry = multi) |>
+    multi <- sf::st_combine(all_segments)
+    multi <- sf::st_cast(multi, "MULTILINESTRING")
+    multi_sf <- sf::st_sf(geometry = multi) |>
       sf::st_line_merge()
 
     stiched_df <-
@@ -100,21 +126,21 @@ fwa_stitch_segments <- function(x, ..., tolerance = 5) {
   }
 
   dplyr::bind_rows(stiched_streams) |>
-    dplyr::rename(!!sf_column_name := geometry) |>
+    dplyr::rename(!!sf_column_name := "geometry") |>
     sf::st_set_geometry(sf_column_name)
 }
 
 
 segment_end_to_start_distance <- function(segments) {
 
-  coords_list <- lapply(segments, function(seg) st_coordinates(seg))
-  end_points <- st_sfc(lapply(coords_list[-length(coords_list)], function(x) st_point(x[nrow(x), 1:2])), crs = st_crs(segments))
-  start_points <- st_sfc(lapply(coords_list[-1], function(x) st_point(x[1, 1:2])), crs = st_crs(segments))
+  coords_list <- lapply(segments, function(seg) sf::st_coordinates(seg))
+  end_points <- sf::st_sfc(lapply(coords_list[-length(coords_list)], function(x) sf::st_point(x[nrow(x), 1:2])), crs = sf::st_crs(segments))
+  start_points <- sf::st_sfc(lapply(coords_list[-1], function(x) sf::st_point(x[1, 1:2])), crs = sf::st_crs(segments))
 
 
   distances <- numeric(length(end_points))
   for(i in seq_along(end_points)) {
-    distances[i] <- as.numeric(st_distance(end_points[i], start_points[i], which = "Euclidean"))
+    distances[i] <- as.numeric(sf::st_distance(end_points[i], start_points[i], which = "Euclidean"))
   }
 
   df <- data.frame(
@@ -123,5 +149,5 @@ segment_end_to_start_distance <- function(segments) {
     distance = distances
   )
 
-  return(df)
+  df
 }
