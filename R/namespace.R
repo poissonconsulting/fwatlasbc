4b600ffe5b8584271e174fbe7636c98dda9a6338
Ayla Pearson <ayla@poissonconsulting.ca>
2025-12-11 14:50:17 -0800

    adding dependcies and functions

--- R/namespace.R
+++ R/namespace.R
@@ -2,4 +2,5 @@
 #' @importFrom dplyr all_of any_of across .data left_join matches mutate n rename as_tibble
 #' @importFrom stats approx
 #' @importFrom stringr str_detect str_extract str_replace str_replace_all
+#' @importFrom rlang `:=`
 NULL
